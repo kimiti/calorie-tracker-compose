85d7dedb3427907412b5932f91da6a18a0491b04
kimiti <kimitikevin@gmail.com>
2024-01-06 08:10:00 +0300

    update gradle files

--- onboarding/onboarding_presentation/build.gradle.kts
+++ onboarding/onboarding_presentation/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(project(":core"))
+    implementation(project(":onboarding:onboarding_domain"))
 }
