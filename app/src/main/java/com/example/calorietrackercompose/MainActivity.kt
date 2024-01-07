bb2f390fd434244253b7803009f3f3de64da6b38
kimiti <kimitikevin@gmail.com>
2024-01-07 08:10:00 +0300

    adds update Main Activity

--- app/src/main/java/com/example/calorietrackercompose/MainActivity.kt
+++ app/src/main/java/com/example/calorietrackercompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -32,6 +33,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Row {  }
     Text(
         text = "Hello $name!",
         modifier = modifier
