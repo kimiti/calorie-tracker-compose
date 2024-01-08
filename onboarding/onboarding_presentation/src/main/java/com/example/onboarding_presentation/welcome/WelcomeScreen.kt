d9b2795063de0a337c5f5605a82a334fbec1efe0
kimiti <kimitikevin@gmail.com>
2024-01-08 08:00:00 +0300

    adds onboarding screen

---
+++ onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -0,0 +1,39 @@
+package com.example.onboarding_presentation.welcome
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.core.R
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomeScreen() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.welcome_text),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.displayLarge
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+
+}
