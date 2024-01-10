85611d4f861483da2ba1cb7e5051ad5efb1e44ce
kimiti <kimitikevin@gmail.com>
2024-01-10 08:00:00 +0300

    update global dimens values

--- core-ui/src/main/java/com/example/core_ui/Dimensions.kt
+++ core-ui/src/main/java/com/example/core_ui/Dimensions.kt
@@ -1,5 +1,6 @@
 package com.example.core_ui
 
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -12,3 +13,5 @@ data class Dimensions(
     val spaceLarge: Dp = 32.dp,
     val spaceExtraLarge: Dp = 64.dp
 )
+
+val LocalSpacing = compositionLocalOf { Dimensions() }
