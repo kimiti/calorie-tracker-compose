c8c4adc3739fc83f62cea15d76185434ad4769e0
kimiti <kimitikevin@gmail.com>
2024-01-11 08:00:00 +0300

    update theme

--- app/src/main/java/com/example/calorietrackercompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/calorietrackercompose/ui/theme/Theme.kt
@@ -9,7 +9,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
+import com.example.core_ui.Dimensions
+import com.example.core_ui.LocalSpacing
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -49,10 +52,12 @@ fun CalorieTrackerComposeTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+    CompositionLocalProvider(LocalSpacing provides Dimensions()) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
 }
