261203d597dce27e8ed7fdbc783780d36b265aa2
kimiti <kimitikevin@gmail.com>
2024-01-30 08:10:00 +0300

    add navigation to main activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,9 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
+
+    implementation("androidx.navigation:navigation-compose:2.9.7")
+
     implementation(project(":core"))
     implementation(project(":core-ui"))
 
--- app/src/main/java/com/example/calorietrackercompose/MainActivity.kt
+++ app/src/main/java/com/example/calorietrackercompose/MainActivity.kt
@@ -12,7 +12,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.calorietrackercompose.navigation.navigate
 import com.example.calorietrackercompose.ui.theme.CalorieTrackerComposeTheme
+import com.example.core.navigation.Route
+import com.example.onboarding_presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,10 +27,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             CalorieTrackerComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME
+                    ){
+                        composable(Route.WELCOME) {
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
+                    }
                 }
             }
         }
